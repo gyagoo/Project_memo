40770b6aae11874f2ffe6b6da5c1079c70481080
gyagoo <ish96116@gmail.com>
2022-01-21 20:06:42 +0900

    22-01-21

---
+++ src/main/java/com/project/memo/ProjectMemoApplication.java
@@ -0,0 +1,13 @@
+package com.project.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMemoApplication.class, args);
+	}
+
+}
